785cbdd3266ae295deeee0a060961408e753e96c
Gitae Kim <gitkim@c3r12s3.42gyeongsan.kr>
2025-07-02 04:01:44 +0900

    JWT base

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,9 @@
-export default async function (fastify) {
+import authRoutes from "./authRoute.js";
+import jwtPlugin from "../shared/plugins/jwt.js";
+
+export default async function routes(fastify) {
+  fastify.register(jwtPlugin);
+  fastify.register(authRoutes);
   fastify.get("/health", async (_request, _reply) => {
     return { status: "ok" };
   });
---
+++ src/shared/plugins/jwt.js
@@ -0,0 +1,16 @@
+import fp from "fastify-plugin";
+import fastifyJwt from "@fastify/jwt";
+
+export default fp(async (fastify) => {
+  fastify.register(fastifyJwt, {
+    secret: process.env.JWT_SECRET || "your-secret-key",
+  });
+
+  fastify.decorate("authenticate", async function (request, reply) {
+    try {
+      await request.jwtVerify();
+    } catch (err) {
+      reply.code(401).send({ message: "Unauthorized" });
+    }
+  });
+});
