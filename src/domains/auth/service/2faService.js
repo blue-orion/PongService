06ef4570e748773a8826ddbc60242164b0f2040f
gitkim95 <qsawed1003@gmail.com>
2025-07-13 01:39:32 +0900

    refactor: separate 2FA functions and structs

--- src/domains/auth/service/2faService.js
+++ src/domains/auth/service/2faService.js
@@ -1,9 +1,34 @@
 import speakeasy from "speakeasy";
 import qrcode from "qrcode";
 
+import userRepo from "#domains/user/repo/userRepo.js";
 import PongException from "#shared/exception/pongException.js";
 
 const twoFAService = {
+  async setup2FA(username) {
+    const user = userRepo.getUserByUsername(username);
+    if (!user) throw new PongException.NOT_FOUND();
+
+    const { secret, qrCodeDataURL } = await generate2FASecret(username);
+    await userRepo.updateUser2FASecret(user.id, secret);
+    return { qrCodeDataURL };
+  },
+
+  async generate2FASecret(username) {
+    const secret = speakeasy.generateSecret({ name: `MyApp (${username})` });
+    const otpauthUrl = secret.otpauth_url;
+    const qrCodeDataURL = await qrcode.toDataURL(otpauthUrl);
+    return { secret: secret.base32, otpauthUrl, qrCodeDataURL };
+  },
+
+  async verifyUser2FA(username, token) {
+    const user = await userRepo.getUserByUsername(username);
+    if (!user || !user.twoFASecret) throw new PongException.ENTITY_NOT_FOUNT();
+
+    const verified = twoFAService.verify2FACode(username, token);
+    return true;
+  },
+
   verify2FACode(secret, token) {
     if (!secret) return;
     if (!token) throw new PongException("2FA code required", 400);
@@ -21,17 +46,3 @@ const twoFAService = {
 };
 
 export default twoFAService;
-
-// 2FA 시크릿 생성 및 QR코드 데이터 반환
-export async function generate2FASecret(username) {
-  const secret = speakeasy.generateSecret({
-    name: `MyApp (${username})`,
-  });
-  const otpauthUrl = secret.otpauth_url;
-  const qrCodeDataURL = await qrcode.toDataURL(otpauthUrl);
-  return {
-    secret: secret.base32,
-    otpauthUrl,
-    qrCodeDataURL,
-  };
-}
